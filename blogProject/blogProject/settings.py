c82410da3b678fc5acab16091859eaff8c3e8d70
Favour okerri <favourokerri767@gmail.com>
2023-12-11 20:59:09 -1100

    .\requirements.txt

--- blogProject/blogProject/settings.py
+++ blogProject/blogProject/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xsg5v4f_3ec$@bmk=t*lbho0vjgcn0$yl4q9+pcf^_gu_&i9+*'
+#SECRET_KEY = 'django-insecure-xsg5v4f_3ec$@bmk=t*lbho0vjgcn0$yl4q9+pcf^_gu_&i9+*'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-xsg5v4f_3ec$@bmk=t*lbho0vjgcn0$yl4q9+pcf^_gu_&i9+*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
