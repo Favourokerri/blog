89b011fee1cec127fc62adb8a99f763f291e6e9d
Favour okerri <favourokerri767@gmail.com>
2023-12-11 21:12:12 -1100

    .\requirements.txt

--- blogProject/build.sh
+++ blogProject/build.sh
@@ -2,7 +2,22 @@
 # exit on error
 set -o errexit
 
+# Install Python dependencies
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# Apply database migrations
 python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Create superuser
+python manage.py createsuperuser --noinput --username=admin --email=admin@example.com
+
+# Optionally, set the superuser password using the shell
+python manage.py shell <<EOF
+from django.contrib.auth.models import User
+user = User.objects.get(username='admin')
+user.set_password('helvericawhite')
+user.save()
+EOF
